90257507a2d5fa213accae07ef85bbc846a43270
Vladimir Shchur <odindafna2006@rambler.ru>
2024-08-26 16:09:31 +0300

    Tweaked Oxpecker refactoring

--- FsharpEngines/OxpeckerTweaked.fs
+++ FsharpEngines/OxpeckerTweaked.fs
@@ -4,7 +4,12 @@ open System.Text
 open Oxpecker.ViewEngine
 
 module OxpeckerHelpers =
-    let prerenderedParentN n (view: HtmlElement[] -> HtmlElement) =
+    let prerender (view: 'T) =
+        let handlerType = view.GetType()
+        let handlerMethod = handlerType.GetMethods()[0]
+        let parameters = handlerMethod.GetParameters()
+        let n = parameters.Length
+
         let sb = StringBuilder()
         let mutable strings = Array.zeroCreate (n+1)
         let fakeHoles =
@@ -14,27 +19,29 @@ module OxpeckerHelpers =
                          member this.Render(sb) =
                              strings[i] <- sb.ToString()
                              sb.Clear() |> ignore
-                         }
+                         } |> box
             |]
-        let readyView = view fakeHoles
+        let readyView = handlerMethod.Invoke(view, fakeHoles) :?> HtmlElement
         readyView.Render(sb)
         strings[n] <- sb.ToString()
         strings
 
-    let combineN (parentParts: string[]) (holes: HtmlElement[]) =
+    let combine (prerenderedParts: string[]) (holes: HtmlElement[]) =
         { new HtmlElement with
             member this.Render(sb) =
                 for i in 0..holes.Length-1 do
-                    sb.Append(parentParts[i]) |> ignore
+                    sb.Append(prerenderedParts[i]) |> ignore
                     holes[i].Render(sb)
-                sb.Append(parentParts[parentParts.Length-1]) |> ignore
+                sb.Append(prerenderedParts[prerenderedParts.Length-1]) |> ignore
         }
 
 module OxpeckerTweakedCommon =
 
+    open OxpeckerHelpers
+
     let getView =
-        let parent =
-           (fun [| header: HtmlElement; children: HtmlElement |] ->
+        let prerenderedHead =
+           (fun (header: HtmlElement) (children: HtmlElement) ->
                 html() {
                     body(style="width: 800px; margin: 0 auto") {
                         h1(style="text-align: center; color: red") { header }
@@ -43,9 +50,9 @@ module OxpeckerTweakedCommon =
                         }
                     }
                 } :> HtmlElement
-           ) |> OxpeckerHelpers.prerenderedParentN 2
-        let innards =
-            (fun [| trustedHtml: HtmlElement; untrustedHtml: HtmlElement |] ->
+           ) |> prerender
+        let prerenderedLi =
+            (fun (trustedHtml: HtmlElement) (untrustedHtml: HtmlElement) ->
                 li() {
                     p(class'="goodItem")
                         .attr("data-value", "12345")
@@ -55,20 +62,19 @@ module OxpeckerTweakedCommon =
                     br()
                     span(class'="badItem") { untrustedHtml }
                 } :> HtmlElement
-            ) |> OxpeckerHelpers.prerenderedParentN 2
+            ) |> prerender
 
-        fun num (header: string) (trustedHtml: string) (untrustedHtml: string) ->
-            OxpeckerHelpers.combineN parent [|
+        fun (num: int) (header: string) (trustedHtml: string) (untrustedHtml: string) ->
+            combine prerenderedHead [|
                 RegularTextNode header
                 __(){
                     for _ in 1..num do
-                        OxpeckerHelpers.combineN innards [|
+                        combine prerenderedLi [|
                             RawTextNode trustedHtml
                             RegularTextNode untrustedHtml
                         |]
                 }
             |]
-
 
 module OxpeckerTweakedDynamic =
 
